402543c3bfb677b8b6de02b82ab971a5a6091108
Leon Yang <leonyang@Leons-MacBook-Air.local>
2026-04-02 23:31:40 -0400

    first commit

---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className = "fixed inset-x-0 z-20 w-full backdrop-blur-lg
+     bg-primary/40">
+        <div className = "mx-auto c-space max-w-7wl">
+            <div className = "flex items-center justify-between py-2 
+            sm:py-0">
+                <a className = "text-xl font-bold transition-colors
+                text-neutral-400 hover:text-white">
+                    Leon
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar
