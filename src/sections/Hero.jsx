c48d7fa7d2db8b320861929375e5ed4f00ea6a1a
Leon Yang <leonyang@Leons-MacBook-Air.local>
2026-04-27 20:51:58 -0400

    hero section finished

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,11 +1,14 @@
 import { Canvas, useFrame } from "@react-three/fiber";
-import HeroText from "../components/HeroText";
-import ParallaxBackground from "../components/ParallaxBackground";
-import Astronaut from "../components/Astronaut";
-import { OrbitControls } from "@react-three/drei";
 import {useMediaQuery} from "react-responsive";
 import { easing } from "maath";
 import { Float } from "@react-three/drei";
+import { Suspense } from "react";
+
+import HeroText from "../components/HeroText";
+import ParallaxBackground from "../components/ParallaxBackground";
+import Astronaut from "../components/Astronaut";
+import Loader from "../components/Loader.jsx"
+
 
 const Hero = () => {
     const isMobile = useMediaQuery({maxWidth: 853});
@@ -18,13 +21,15 @@ const Hero = () => {
         style={{ width: "100vw", height: "100vh" }}
       >
         <Canvas camera = {{position: [0, 1, 3]}} >
-            <Float>
-                <Astronaut 
-                scale={isMobile && 0.23} 
-                position = {isMobile && [0, -1.5, 0]} 
-                />
-            </Float>
-          <Rig />
+            <Suspense fallback = {<Loader />}>
+                <Float>
+                    <Astronaut 
+                    scale={isMobile && 0.23} 
+                    position = {isMobile && [0, -1.5, 0]} 
+                    />
+                </Float>
+                <Rig />
+          </Suspense>
         </Canvas>
       </figure>
     </section>
